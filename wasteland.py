401b6bb3f8dbb2ce2c750aa6c40743254f03e808
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-06-14 01:17:19 +0200

    fix wasteland note transcription errors

--- wasteland.py
+++ wasteland.py
@@ -14,7 +14,7 @@ wasteland_notes = (
     "R.5 R.12 T.38 R.4 R.13 T.2 A.1 T.13 T.24 A.8 A.7 A.2",
     "Q.1 Q.17 T.3 Q.12 Q.6 R.11 R.12 R.13 T.18 Q.5 Q.13 Q.8 Q.16 T.2 T.31 T.7 A.8 Q.3 Q.1 Q.3 T.25 Q.6 T.13 Q.11 Q.4 Q.6 Q.11 Q.2 Q.9 SURFACE",
     "EVERYONE Q.2 Q.9 A.5 A.4 A.7 Q.7 A.6 A.2 Q.3 Q.14 R.3 T.27 R.5 T.28 T.28 T.23 T.30 R.7 A.4 A.5 A.4",
-    "T.19 T.15 T.9 T.17 R.13 T.20 T.14 T.20 T.4 T.6 T.29 T.23 T.50 T.18 T.26 T.5 T.13 T.24 A.8 T.25 T.36 T.28 R.5 T.8 T.5 T.7 A.8",
+    "T.19 T.15 T.9 T.17 R.13 T.20 T.14 T.20 T.4 T.6 T.29 T.23 T.50 T.18 T.26 T.5 T.13 T.24 A.8 T.23 T.36 T.28 R.5 T.8 T.5 T.7 A.8",
     "T.28 T.22 T.4 T.14 T.11 Q.1 Q.6 A.5 Q.14 A.8 Q.5 Q.6 Q.7 Q.8 Q.9 Q.10 Q.11 Q.20 T.35 A.2 A.1 A.2 A.3 A.4 WRONG WORLD",
     "Q.1 Q.17 Q.20 Q.7 T.25 Q.6 T.13 A.8 A.7 A.9 YOU CAN'T GET OUT",
     "EVERY WORLD HAS OWN LAWS THEY JUST HAVE TO BE CORRECT ONES",
@@ -26,7 +26,7 @@ wasteland_notes = (
     "A.8 A.7 A.8 R.6 R.7 R.1 R.10 R.3 A.1 R.11 R.3 A.8 R.4 A.6 R.10 R.5 A.1 T.18 EVERYTHING",
     "THE PLACE OUTSIDE THE WORLDS",
     "THE BORDER OF THE CHAOS IT IS THIS WORLD",
-    "A.3 A.1 Q.1 R.11 T.17 R.13 T.25 T.11 T.10 R.11 R.4 T.14 T.25 A.1 T.5 R.7 R.12 T.36 R.4 A.6 A.9 A.9 Q.3 A.2 A.3 A.7 A.6 R.3 R.2 R.12 Q.5 T.6 Q.1 Q.6 A.5 Q.14 A.8",
+    "A.3 A.1 Q.1 R.11 T.17 R.13 T.25 T.11 T.10 R.11 R.4 T.14 T.25 A.1 T.5 R.7 R.12 T.36 R.4 A.6 A.9 A.9 Q.7 A.2 A.3 A.7 A.6 R.3 R.2 R.12 Q.5 T.6 Q.1 Q.6 A.5 Q.14 A.8",
     "WITHOUT THE CORRECT LAWS NOTHING STAYS TOGETHER",
     "THE MACHINE Q.5 Q.3 T.27 Q.12 Q.9 Q.19 A.7 A.2 A.2 A.3 A.9 T.3 A.8 A.1 Q.7 A.2 T.6 Q.7 Q.6 Q.1 A.7 A.2",
     "A.7 A.6 A.2 T.22 A.7 A.6 Q.3 Q.14 Q.15 T.12 T.5 Q.12 Q.11 Q.3 Q.14 A.7 A.4",
@@ -43,14 +43,17 @@ wasteland_notes = (
 )
 
 
-def decode_wasteland_message(key: Dict[str, str], message: str) -> str:
+def decode_wasteland_message(
+    key: Dict[str, str], message: str, with_code: bool = False
+) -> str:
     def decode_word(word: str) -> str:
         if "." not in word:
             return word
 
         prefix, index = word.split(".")
         try:
-            return key[prefix][int(index) - 1]
+            decoded = key[prefix][int(index) - 1]
+            return f"{decoded}={word}" if with_code else decoded
         except IndexError:
             return word
 
