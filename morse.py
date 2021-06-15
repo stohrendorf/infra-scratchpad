083f942bbd6d81013279ae0fe627eefba1fe7365
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-06-16 00:21:39 +0200

    add morse codes

---
+++ morse.py
@@ -0,0 +1,62 @@
+from typing import Tuple
+
+WORD_DELIMITER = "#"
+SENTENCE_DELIMITER = "###"
+
+morse = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0",
+    ".-.-.-": ".",
+}
+
+assert len(set(morse.values())) == len(morse), f"Morse code mappings are not unique"
+
+
+def decode_morse(encoded: str) -> Tuple[str]:
+    def decode_word(encoded_word: str) -> str:
+        return "".join(morse[char] for char in encoded_word.split())
+
+    def decode_sentence(encoded_sentence: str) -> str:
+        return " ".join(
+            decode_word(encoded_word)
+            for encoded_word in encoded_sentence.split(WORD_DELIMITER)
+        )
+
+    return tuple(
+        decode_sentence(encoded_sentence.strip())
+        for encoded_sentence in encoded.split(SENTENCE_DELIMITER)
+    )
---
+++ wasteland_radio_morse.py
@@ -0,0 +1,12 @@
+from morse import decode_morse
+
+wasteland_radio_morse = (
+    ".-. .- ...- . -. .-.-.- # --- - .... . .-."
+    " ### ... .. -.. . .-.-.- # .-. . ... . .- .-."
+    " ### ... - --- .--. .--. . -.."
+)
+
+
+if __name__ == "__main__":
+    for sentence in decode_morse(wasteland_radio_morse):
+        print(sentence)
