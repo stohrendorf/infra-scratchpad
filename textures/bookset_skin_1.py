7c2e04f434154e5c30b7dcb0e4d690b7d63be27d
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-06-18 07:47:50 +0200

    add bookset_skin_1

---
+++ textures/bookset_skin_1.py
@@ -0,0 +1,8 @@
+bookset_skin_1 = {
+    1: "E",  # bookset_skin1
+    2: "C",  # bookset_skin1; large_transformer
+    3: "N",  # electricalbox_006_skin1
+    5: "K",  # electricalbox_006_skin1
+    8: "H",  # bookset_skin1
+    9: "S",  # bookset_skin1
+}
