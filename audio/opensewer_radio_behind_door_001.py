39f3849e2a74bdaa0981d5ace410df6c5d2eeb47
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-06-19 12:08:54 +0200

    add opensewer_radio_behind_door_001

---
+++ audio/opensewer_radio_behind_door_001.py
@@ -0,0 +1,16 @@
+radio_behind_door_001_transcription = (
+    "0 11 Grad 17,40 Minuten Ost."
+    " Die Schifffahrt wird gebeten mit einem Abstand von 0,5 Seemeilen zu ..."
+    " Und nun noch die Seewarnnachrichten für die Ostsee."
+    " Westliche Ostsee: Fehmarn, Puttgarden."
+    " Unterwasserarbeiten durch Deep River, Verzeihung, Deep Diver 1 auf Position 54 Grad, 33,49 Minuten Nord"
+)
+
+# translated using deepl.com
+radio_behind_door_001_transcription_translated = (
+    "0 11 degrees 17.40 minutes east."
+    " Shipping is requested to keep a distance of 0.5 nautical miles from ..."
+    " And now the sea warning messages for the Baltic Sea."
+    " Western Baltic Sea: Fehmarn, Puttgarden."
+    " Underwater work by Deep River, excuse me, Deep Diver 1 at position 54 degrees, 33.49 minutes north"
+)
